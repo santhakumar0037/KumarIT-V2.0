c6d7c6a8f0154fade009f5a083c757a01ae2fc9e
Santha kumar S <santhakumar0037@gmail.com>
2019-02-22 17:22:40 +1100

    KumarResumeAPI added and CvServiceController created.

---
+++ KumarIT/KumarResumeAPI/Controllers/CvServiceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+
+namespace KumarResumeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CvServiceController : ControllerBase
+    {
+        const string DownloadUrl = "https://s3-ap-southeast-2.amazonaws.com/s.santhakumar/Resume/Santha+kumar+Resume.docx";
+        const string WordContentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+            [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+
+            var httpClient = new HttpClient();
+
+            var httpResponseMessage = await httpClient.GetStreamAsync(DownloadUrl);
+
+            return File(httpResponseMessage, WordContentType);
+        }
+    }
+}
+}
